79f6dbeef01178e8e21a8db0f5008b96d0c7456a
JearlMNS <jearlmilesnamit@gmail.com>
2026-08-27 10:05:05 +0800

    Update greetSystem_script.js

--- greetSystem_script.js
+++ greetSystem_script.js
@@ -1,1 +1,16 @@
+const nameINPUT = document.getElementById("studentName");
+const greetButton = document.getElementById("greetBtn");
+const output = document.getElementById("output");
 
+greetButton.addEventListener("click", function() {
+    const name = nameINPUT.value.trim();
+
+    if (name !== "") {
+        output.textContent = `Hello, ${name}! Welcome to EVENT DRIVEN WEB DEVELOPMENT!`;
+
+    }
+});
+
+nameINPUT.addEventListener("input", function() {
+    output.textContent = "";
+});
